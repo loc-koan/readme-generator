6ac14139b27ac96d3e849f5b46aa8b0b5fbd5661
loc-koan <loc.koan@yahoo.com>
2020-06-14 19:42:13 -0500

    filled out markdown file

--- markdown.js
+++ markdown.js
@@ -1,17 +1,40 @@
 /* function to generate markdown for README */
 function generateMarkdown(userAnswers) {
-    return # ${userAnswers.repoTitle}
+    return 
+    `# ${userAnswers.repoTitle}
 
     ## Description:
     ${userAnswers.repoDescription}
-}
 
-* Title
-* Description
-* Table of Contents
-* Installation
-* Usage
-* License
-* Contributing
-* Tests
-* Questions
+    ## Table of Contents:
+    * [Description] (#Description)
+    * [Table of Contents] (#Table of Contents)
+    * [Installation] (#Installation)
+    * [Usage] (#Usage)
+    * [License] (#License)
+    * [Contributing] (#Contributing)
+    * [Tests] (#Tests)
+    * [Questions] (#Questions)
+
+    ## Installation:
+    ${userAnswers.repoInstallation}
+
+    ## Usage:
+    ${userAnswers.repoUsage}
+
+    ## License:
+    ${userAnswers.repoLicense}
+
+    ## Contributing:
+    ${userAnswers.repoContribution}
+
+    ## Tests:
+    ${userAnswers.repoTesting}
+
+    ## Questions:
+    If you have any questions, I can be reached at ${userAnswers.repoGithub} or ${userAnswers.repoEmail}
+    `;
+
+}
+/* exports this file to be used by the index file */
+module.exports = generateMarkdown;
