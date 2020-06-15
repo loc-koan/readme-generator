2efa9edfac53e50877010d3557715429166728d8
loc-koan <loc.koan@yahoo.com>
2020-06-14 19:26:52 -0500

    start of markdown file

---
+++ markdown.js
@@ -0,0 +1,17 @@
+/* function to generate markdown for README */
+function generateMarkdown(userAnswers) {
+    return # ${userAnswers.repoTitle}
+
+    ## Description:
+    ${userAnswers.repoDescription}
+}
+
+* Title
+* Description
+* Table of Contents
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
